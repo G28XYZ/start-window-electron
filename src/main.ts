3c31484f436e0e8582dcc998108e164c2b47de88
g28xyz <g28xyz@ya.ru>
2025-06-20 00:41:55 +0700

    иконки в public

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ const createWindow = () => {
 		frame         : false,
 		transparent   : true,
 		webPreferences: { preload: path.join(__dirname, 'preload-start.js') },
-		icon          : path.join(__dirname, '..', '..', 'src', 'app', 'images', 'rBmc.png')
+		icon          : path.join(__dirname, 'rBmc.png')
   });
 
 	if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
@@ -64,7 +64,7 @@ ipcMain.on('toggle-start-window', () => {
 		y             : mainY - winHeight - 25,
 		title         : 'Start Window',
 		webPreferences: { preload: path.join(__dirname, 'preload-window.js') },
-		icon          : path.join(__dirname, '..', '..', 'src', 'app', 'images', 'rBmc.png')
+		icon          : path.join(__dirname, 'rBmc.png')
 	});
 
 
